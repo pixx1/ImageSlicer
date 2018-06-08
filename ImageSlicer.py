604e3a849a8b8f2fa00d9d4323952f68bb7300b2
Jonas Schober <s67635@beuth-hochschule.de>
2018-06-08 15:56:17 +0200

    added new function, "bynumber"

--- ImageSlicer.py
+++ ImageSlicer.py
@@ -10,10 +10,11 @@ from typing import List, Dict
 import ntpath
 
 
-def bysize(image_path: str, width_new_image: int,
+def bysize(image_path: str,
+           width_new_image: int,
            height_new_image: int,
-           output_folder: str ="sliced/",
-           keep_end_sections: bool =True) -> None:
+           output_folder: str="sliced/",
+           keep_end_sections: bool=True) -> None:
     """
     function for slicing images into smaller pieces.
     :param image_path: path to image file
@@ -134,11 +135,7 @@ def bysize(image_path: str, width_new_image: int,
 def bynumber(image_path: str,
              width: int,
              height: int,
-             output_folder="sliced/"):
-
-    if not os.path.isfile(image_path):
-        print("Input is not a file. Exit")
-        exit()
+             output_folder="sliced/", keep_end_sections: bool=True):
 
     def __input_validation(input_value):
 
@@ -148,14 +145,19 @@ def bynumber(image_path: str,
 
     def __box_size(image_size, box_size):
         side = round(image_size / box_size)
-        print(side)
-        print(side * width)
+        return side
+
+    if not os.path.isfile(image_path):
+        print("Input is not a file. Exit")
+        exit()
 
     __input_validation(width)
     __input_validation(height)
 
     with Image.open(image_path) as im:
         width_of_the_image, height_of_the_image = im.size
-        print(width_of_the_image)
 
-        __box_size(width_of_the_image, width)
+        width_for_splitting = __box_size(width_of_the_image, width)
+        height_for_splitting = __box_size(height_of_the_image, height)
+
+        bysize(image_path, width_for_splitting, height_for_splitting, output_folder, keep_end_sections)
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import ImageSlicer
 
 if __name__ == '__main__':
     # ImageSlicer.bysize("example.jpg", 2000, 2000, output_folder="slice_image", keep_end_sections=True)
-    ImageSlicer.bynumber("example.jpg", 200, 100)
+    ImageSlicer.bynumber("example.jpg", 10, 10)
